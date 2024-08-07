21aefcf92a2fd5bb3eb2c54a69a6a8c122405daf
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2024-08-07 10:18:22 +0000

    updation

---
+++ music-club/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen bg-black/[0.96] antialiased bg-grid-white flex-col items-center justify-between p-24">
+    </main>
+  );
+}
