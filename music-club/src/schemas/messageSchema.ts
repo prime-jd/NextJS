605c7ef57747bbc267e5ac1b9a333271b72e3a5d
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2024-08-17 18:49:04 +0000

    message Schema

--- music-club/src/schemas/messageSchema.ts
+++ music-club/src/schemas/messageSchema.ts
@@ -2,4 +2,4 @@ import {z} from 'zod'
 
 export const messageSchema = z.object({
     content : z.string().min(10, {message : "Content must be of at least 10 Characters"}).max(100, "Content not more than 100 Characters")
-})
+}) 
