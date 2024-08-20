46907a949ec639ce30685428959ee8d35fe914eb
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2024-08-20 19:35:46 +0000

    resend mail verification

--- music-club/src/helpers/sendVerificationEmail.ts
+++ music-club/src/helpers/sendVerificationEmail.ts
@@ -1,5 +1,5 @@
 import { resend } from "@/lib/resend";
-import VerificationEmail from "../../emails/VerificationEmail";
+import VerificationEmail from "../../email/VerificationEmail";
 import { ApiResponse } from '@/types/ApiResponse';
 
 export async function sendVerificationEmail(
