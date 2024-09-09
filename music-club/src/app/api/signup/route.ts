a699af49a2425b50a1651dfe8c06899deb22273f
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2024-09-09 18:14:58 +0000

    verify code

--- music-club/src/app/api/signup/route.ts
+++ music-club/src/app/api/signup/route.ts
@@ -33,6 +33,7 @@ export async function POST(request : Request){
                 email,
                 username,
                 password : hashedPassword,
+                verifyCode
             })
         }
         
